7a0b3b8820b9d6d4e989b05b1fbb772cd3553992
OmnioFarm <135119878+OmnioFarm@users.noreply.github.com>
2025-09-30 16:14:19 +0200

    Fix ACT notifications conditions column

--- OmniAPI/Models/ActNotificationDto.cs
+++ OmniAPI/Models/ActNotificationDto.cs
@@ -5,7 +5,7 @@ namespace OmniAPI.Models
         public int Id { get; set; }
         public string Act { get; set; }
         public bool Completed { get; set; }
-        public bool CompletedNotMet { get; set; }
+        public bool ConditionsNotMet { get; set; }
         public bool NotCompleted { get; set; }
         public string PrimaryContact { get; set; }
         public string SecondaryContact { get; set; }
